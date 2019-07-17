4eabb8e04ef13670f25e7126013ecc8c8aa8df68
Byron Varberg <byron@crema.us>
2019-07-17 16:20:20 -0500

    Implement VS Code versions of Rack and Indicator
    These implementations are untested, but provide the actual
    functionality for the extension. At this point, the tests verify and
    solidify the putOnAHat command functions contract with its
    collaborators. The value from the tests came mostly from using them as a
    tool for thinking about the design (not that that turned out to be
    particularly good...).

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,13 +1,29 @@
 import * as vscode from "vscode"
+import putOnAHat from "./commands/putOnAHat"
+import { StatusBarIndicator } from "./StatusBarIndicator"
+import { QuickPickRack } from "./QuickPickRack"
+
+enum CommandID {
+  PutOnAHat = "twoHats.putOnAHat",
+}
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log('Congratulations, your extension "two-hats" is now active!')
+  const hats = [
+    {
+      name: "Adding functionality",
+    },
+    {
+      name: "Refactoring",
+    },
+  ]
+  const rack = QuickPickRack.fromHats(hats)
+  const indicator = new StatusBarIndicator()
 
-  let disposable = vscode.commands.registerCommand("twoHats.putOnAHat", () => {
-    vscode.window.showInformationMessage("Hello VS Code")
-  })
+  const boundPutOnAHat = () => putOnAHat(rack, indicator)
 
-  context.subscriptions.push(disposable)
+  context.subscriptions.push(
+    vscode.commands.registerCommand(CommandID.PutOnAHat, boundPutOnAHat),
+  )
 }
 
 export function deactivate() {}
