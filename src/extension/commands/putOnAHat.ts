5f2c6b7252206553fc13c0045574f21bb4996e8f
Byron Varberg <byron@crema.us>
2019-07-17 11:58:06 -0500

    Implements putOnAHat to use the indicator

--- src/extension/commands/putOnAHat.ts
+++ src/extension/commands/putOnAHat.ts
@@ -1,3 +1,5 @@
 import { Indicator } from "../types/Indicator"
 
-export default function putOnAHat(indicator: Indicator) {}
+export default function putOnAHat(indicator: Indicator) {
+  indicator.setText("put on a hat")
+}
